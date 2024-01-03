00a84938bdc97a04586c93fc3ba1210fc34bfb07
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2024-01-03 09:50:56 -0300

    ex2

--- dist/js/ex02.js
+++ dist/js/ex02.js
@@ -1,4 +1,12 @@
 "use strict";
+let padraoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function validarEmail(email) {
+    if (!padraoEmail.test(email)) {
+        return false;
+    }
     return true;
 }
+console.log(validarEmail("Olá"));
+console.log(validarEmail("eduardofrancisc99@gmail.com"));
+console.log(validarEmail("5e2c5b659557f1@crankymonkey.info"));
+console.log(validarEmail("7d556a65955806@cashbenties.com"));
--- src/ex02.ts
+++ src/ex02.ts
@@ -1,7 +1,14 @@
 //Implemente uma função que recebe um e-mail como parâmetro e retorna true se for um formato de e-mail válido e false caso contrário.
+let padraoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function validarEmail(email: string): boolean {
-    
-    
+    if(!padraoEmail.test(email)){
+        return false
+    }
     return true
 }
+
+console.log(validarEmail("Olá"))
+console.log(validarEmail("eduardofrancisc99@gmail.com"))
+console.log(validarEmail("5e2c5b659557f1@crankymonkey.info"))
+console.log(validarEmail("7d556a65955806@cashbenties.com"))
